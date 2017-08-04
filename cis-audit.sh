fb6b7b58b703ecdbdae00c2e94c161854aa7267c
Andy Dustin <andy.dustin@touchpoint.co.nz>
2017-08-04 19:01:28 +1200

    Added license

--- cis-audit.sh
+++ cis-audit.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 ## andy.dustin@gmail.com [rev: a42d1a0]
+
 ## This script checks for compliance against CIS CentOS Linux 7 Benchmark v2.1.1 01-31-2017 measures
 ## Each individual standard has it's own function and is forked to the background, allowing for 
 ## multiple tests to be run in parallel, reducing execution time.
 
+##
+## Copyright 2017 Andy Dustin
+##
+## Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except 
+## in compliance with the License. You may obtain a copy of the License at
+##
+## http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software distributed under the License is 
+## distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and limitations under the License.
+##
 
 ### Variables ###
 ## This section defines global variables used in the script
